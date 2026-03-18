2f859aa0a9e1bd6c1c6db4ca7755b3e24a126c39
matthewvidacovich <194949094+matthewvidacovich@users.noreply.github.com>
2026-03-18 18:03:50 -0500

    wip: hoist is logged in to router

--- Selu383.SP26.Web/src/Router.tsx
+++ Selu383.SP26.Web/src/Router.tsx
@@ -1,12 +1,21 @@
 import { Routes, Route } from "react-router";
-import App from "@/pages/App";
+import Home from "@/pages/Home";
 import { TestPage } from "@/pages/TestPage";
+import { UserLoggedInContext } from "@/context/UserLoggedInContext";
+import { useState } from "react";
 
 export function Router() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
-    <Routes>
-      <Route index element={<App />} />
-      <Route path="test" element={<TestPage />} />
-    </Routes>
+    <>
+      <button onClick={() => setIsLoggedIn((isLoggedIn) => !isLoggedIn)}>{isLoggedIn ? "Log out" : "Log in"}</button>
+
+      <UserLoggedInContext.Provider value={isLoggedIn}>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="test" element={<TestPage />} />
+        </Routes>
+      </UserLoggedInContext.Provider>
+    </>
   );
 }
