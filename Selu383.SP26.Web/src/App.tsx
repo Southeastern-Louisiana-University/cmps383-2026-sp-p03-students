a11fb2916bcdae9367aa2dfda92a9f2343576ab2
matthewvidacovich <194949094+matthewvidacovich@users.noreply.github.com>
2026-03-18 17:06:29 -0500

    chore: install react router
    fix: compile errors on web

--- Selu383.SP26.Web/src/App.tsx
+++ Selu383.SP26.Web/src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { IncrementCountButton, UncontrolledIncrementCountButton } from "./IncrementCountButton";
+import { UncontrolledIncrementCountButton } from "./IncrementCountButton";
 import { UserLoggedInContext } from "./UserLoggedInContext";
 
 interface LocationDto {
